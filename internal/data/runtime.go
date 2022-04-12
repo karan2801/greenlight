f88d537cda0091c2850be9beb9375f7d426ee978
Karan Gupta <guptakaran725@gmail.com>
2022-04-12 17:59:41 +0530

    Added validator class to process requests

--- internal/data/runtime.go
+++ internal/data/runtime.go
@@ -1,9 +1,13 @@
 package data
 
 import (
+	"errors"
 	"fmt"
 	"strconv"
+	"strings"
 )
+
+var ErrInvalidRuntimeFormat = errors.New("invalid runtime format")
 
 type Runtime int32
 
@@ -11,4 +15,21 @@ func (r Runtime) MarshalJSON() ([]byte, error) {
 	json_value := fmt.Sprintf("%d mins", r)
 	quoted_json_value := strconv.Quote(json_value)
 	return []byte(quoted_json_value), nil
+}
+
+func (r *Runtime) UnmarshalJSON(jsonValue []byte) error {
+	unquotedJSONValue, err := strconv.Unquote(string(jsonValue))
+	if err != nil {
+		return ErrInvalidRuntimeFormat
+	}
+	parts := strings.Split(unquotedJSONValue, " ")
+	if len(parts) != 2 || parts[1] != "mins" {
+		return ErrInvalidRuntimeFormat
+	}
+	i, err := strconv.ParseInt(parts[0], 10, 32)
+	if err != nil {
+		return ErrInvalidRuntimeFormat
+	}
+	*r = Runtime(i)
+	return nil
 }
